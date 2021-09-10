238786217c796212b3d0061bc235d5e1bcefd57d
Hudyson Richard dos Santos <2011550343@fema.edu.br>
2021-09-10 00:57:19 -0300

    update entregador

--- Models/Dominio/Entregador.cs
+++ Models/Dominio/Entregador.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace WebAQsede.Models.Dominio
 {
-    [Table("Entregador")]
+    [Table("Entregadores")]
     public class Entregador
     {
         [Key]
@@ -19,41 +19,41 @@ namespace WebAQsede.Models.Dominio
 
         [StringLength(35, ErrorMessage = "Não aceita Nome com mais de 35 dígitos")]
         [DisplayName("Nome")]
-        [Required(ErrorMessage = "Campo nome do cliente é obrigatório")]
+        [Required(ErrorMessage = "Campo nome do entregador é obrigatório")]
         public String Nome { get; set; }
 
         [StringLength(14, ErrorMessage = "Não aceita CPF com mais de 14 dígitos")]
         [DisplayName("CPF")]
-        [Required(ErrorMessage = "Campo CPF do cliente é obrigatório")]
-        [Remote("ValidarCPF", "Clientes", ErrorMessage = "CPF Inválido!!!")]
+        [Required(ErrorMessage = "Campo CPF do entregador é obrigatório")]
+        [Remote("ValidarCPF", "entregador", ErrorMessage = "CPF Inválido!!!")]
         public int CPF { get; set; }
 
         [StringLength(10, ErrorMessage = "DD/MM/AAAA")]
         [DisplayName("Data de Nascimento")]
-        [Required(ErrorMessage = "Campo nome do cliente é obrigatório")]
-        [Remote("ValidarData", "Clientes", ErrorMessage = "Data Inválida!!!")]
+        [Required(ErrorMessage = "Campo nome do entregador é obrigatório")]
+        [Remote("ValidarData", "entregador", ErrorMessage = "Data Inválida!!!")]
         public String DataNascimento { get; set; }
 
 
         [StringLength(35, ErrorMessage = "Não aceita Email com mais de 35 dígitos")]
         [DisplayName("Email")]
-        [Required(ErrorMessage = "Campo Email do cliente é obrigatório")]
+        [Required(ErrorMessage = "Campo Email do entregador é obrigatório")]
         public String Email { get; set; }
 
         [StringLength(10, ErrorMessage = "Não aceita CEP com mais de 10 dígitos")]
         [DisplayName("CEP")]
-        [Required(ErrorMessage = "Campo CEP do cliente é obrigatório")]
+        [Required(ErrorMessage = "Campo CEP do entregador é obrigatório")]
         public int CEP { get; set; }
 
 
         [StringLength(35, ErrorMessage = "Não aceita cidade com mais de 35 dígitos")]
         [DisplayName("Cidade")]
-        [Required(ErrorMessage = "Campo Cidade do cliente é obrigatório")]
+        [Required(ErrorMessage = "Campo Cidade do entregador é obrigatório")]
         public String Cidade { get; set; }
 
         [StringLength(14, ErrorMessage = "Não aceita Nome com mais de 14 dígitos")]
         [DisplayName("Telefone")]
-        [Required(ErrorMessage = "Campo telefone do cliente é obrigatório")]
+        [Required(ErrorMessage = "Campo telefone do entregador é obrigatório")]
         public int Telefone { get; set; }
 
         [Display(Name = "CNH")]
